8b953083226f1c7778fbf41ca063b94d5d531852
Jesco Wuester <jescowuester@gmail.com>
2019-01-13 19:29:08 +0100

    Updates

---
+++ precache-manifest.a5690f788001470ceb633c69f1898cf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7f8b757f233b4d25b88",
+    "url": "/homepage/static/css/main.d65e4d2e.chunk.css"
+  },
+  {
+    "revision": "c7f8b757f233b4d25b88",
+    "url": "/homepage/static/js/main.c7f8b757.chunk.js"
+  },
+  {
+    "revision": "c1a590f322f980f17ca8",
+    "url": "/homepage/static/js/1.c1a590f3.chunk.js"
+  },
+  {
+    "revision": "c9f2de54d44bb9049b00",
+    "url": "/homepage/static/js/runtime~main.c9f2de54.js"
+  },
+  {
+    "revision": "beaab26664137dc78edd6a735b31b40d",
+    "url": "/homepage/static/media/Moon2.0-Regular.beaab266.otf"
+  },
+  {
+    "revision": "d0b26cd963132bab3524eb46a4af5b37",
+    "url": "/homepage/static/media/TomatoSoup-Regular.d0b26cd9.otf"
+  },
+  {
+    "revision": "d44253ddd70db40278658436af33a309",
+    "url": "/homepage/index.html"
+  }
+];
